bcb359f41d6dfa890876d5ce321c90210e9cd414
kanayan_cg <kamal.a.nayan@capgemini.com>
2025-10-08 17:25:33 +0530

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello Marketing</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
